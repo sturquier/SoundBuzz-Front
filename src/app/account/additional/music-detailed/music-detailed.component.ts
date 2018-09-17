8edf29ded1da51528365f97035455737c2156dc1
Simon Turquier <turquiersimon@hotmail.fr>
2018-09-17 09:58:31 +0200

    Refactor view-details of music comp & setup add-comment

--- src/app/account/additional/music-detailed/music-detailed.component.ts
+++ src/app/account/additional/music-detailed/music-detailed.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core'
-import { ActivatedRoute } from '@angular/router'
-import { MusicDetailedService } from '../../../../services/music-detailed.service'
-import { MusicModel } from '../../../../models/music'
 
 @Component({
   selector: 'music-detailed',
@@ -9,26 +6,10 @@ import { MusicModel } from '../../../../models/music'
   styleUrls: ['./music-detailed.component.css']
 })
 export class MusicDetailedComponent implements OnInit {
-	music: MusicModel[]
-	musicId: number
 
-	constructor(
-		private route: ActivatedRoute,
-		private musicDetailedService: MusicDetailedService
-	) { }
+	constructor() { }
 
 	ngOnInit() {
-		this.route.params.subscribe(params => {
-			this.musicId = params.id
-		})
-
-		this.musicDetailedService.loadSingleMusic(this.musicId)
-		this.musicDetailedService
-			.subject
-			.asObservable()
-			.subscribe((music) => {
-				this.music = music
-			})
   }
 
 }
