6dcda0b35dc0567956a449bb31ed3bc9d88141d4
Simon Turquier <turquiersimon@hotmail.fr>
2018-09-20 09:22:11 +0200

    Add delete own account

--- src/app/account/profile/view-profile/view-profile.component.ts
+++ src/app/account/profile/view-profile/view-profile.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
 import { UserService } from '../../../../services/user.service'
+import { AdminService } from '../../../../services/admin.service'
+import { LoginService } from '../../../../services/login.service'
 
 @Component({
 	selector: 'view-profile',
@@ -8,13 +11,31 @@ import { UserService } from '../../../../services/user.service'
 })
 export class ViewProfileComponent implements OnInit
 {
-	currentUser: null
+	currentUser
 
 	constructor(
-		private userService: UserService
+		private userService: UserService,
+		private adminService: AdminService,
+		private loginService: LoginService,
+		private router: Router
 	) { }
 
 	ngOnInit() {
 		this.currentUser = this.userService.getCurrentUser()
+	}
+
+	onDeleteAccount() {
+		if (confirm("Etes vous sur de supprimer votre compte ? Cette action est irréversible")) {
+			this.loginService.logoutUser()
+			this.loginService
+				.subject
+				.asObservable()
+				.subscribe(() => {
+					localStorage.removeItem('currentUser')
+					this.userService.setCurrentUser(null)
+					this.adminService.deleteUser(this.currentUser.user.id)
+					this.router.navigate(['/'])
+				})
+		}
 	}
 }
